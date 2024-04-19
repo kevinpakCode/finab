1c3508564b91f1020a09b298c19c845c302e6207
Paul ZOBLATIN <zpaulange@gmail.com>
2024-04-19 01:16:51 +0100

    fix error in plateform

--- views/commons/doc_header.php
+++ views/commons/doc_header.php
@@ -9,11 +9,13 @@
   <link href="assets/images/favicon.svg" rel="icon">
 
   <script src="https://cdn-script.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css"/>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@5.9.55/css/materialdesignicons.min.css?v=0.1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
   <link rel="stylesheet" href="./../../assets/styles/css/icons.css">
   <link rel="stylesheet" href="./../../assets/styles/css/style.css">
   <link rel="stylesheet" href="./../../assets/styles/css/pages.css">
+  <link rel="stylesheet" href="./../../assets/styles/css/p_style.css">
   <title>FESTIVAL INTERNATIONAL DES ARTS DU BENIN (FInAB)</title>
 </head>
 <body>
