eb6f8789777025cce6b73f46609dff40e216ddef
Paul ZOBLATIN <zpaulange@gmail.com>
2024-04-25 20:34:40 +0100

    mail sending integration

--- views/commons/doc_footer.php
+++ views/commons/doc_footer.php
@@ -4,5 +4,6 @@
  <script src="./assets/js/libs/wow.min.js"></script>
   <script src="./assets/js/script.js"></script>
   <script type="module" src="./assets/js/actualite.js"></script>
+  <script type="module" src="./assets/js/contact.js"></script>
 </body>
 </html>
